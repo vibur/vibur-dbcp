723f66b6e1c2ff27b5c8f2c207bbc1feaa4f6f76
simeon.malchev@gmail.com <simeon.malchev@gmail.com@27e81457-bc9d-6bdf-c01a-22e69fa685e4>
2013-03-29 13:27:43 +0000

    comment
    git-svn-id: https://vibur-dbcp.googlecode.com/svn/trunk@14 27e81457-bc9d-6bdf-c01a-22e69fa685e4

--- src/test/java/vibur/dbcp/ProxyTest.java
+++ src/test/java/vibur/dbcp/ProxyTest.java
@@ -32,6 +32,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
+ * This test uses the <a href="http://dev.mysql.com/doc/sakila/en/">Sakila sample database</a>.
+ * The database has to be installed and setup as described in the previous link.
+ *
  * @author Simeon Malchev
  */
 public class ProxyTest {
