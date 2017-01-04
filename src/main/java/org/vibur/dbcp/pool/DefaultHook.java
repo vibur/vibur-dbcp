1b18a20009cfba3c279dddf26ed36e29ebedca4e
simeonmalchev <simeon.malchev@gmail.com>
2017-01-05 09:38:28 +1100

    fixing comment

--- src/main/java/org/vibur/dbcp/pool/DefaultHook.java
+++ src/main/java/org/vibur/dbcp/pool/DefaultHook.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Simeon Malchev
+ * Copyright 2016 Simeon Malchev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
